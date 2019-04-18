e34f5df3c017abc428b5698b8b71645e6c6b0f11
Demian <demian.dlrc@gmail.com>
2019-04-17 22:07:12 -0300

    Ajustes footer
    Ajustes Css Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,11 @@ import "../App.css";
 
 const Footer = () => {
   return (
-    <MDBFooter color="green darken-2" className="font-smallpt-4 z-depth-3">
+    <MDBFooter color="green darken-2" className="font-small pt-4 z-depth-3">
       <MDBContainer fluid className="text-center text-md-left">
-        <MDBRow className="footer pt-5">
+        <MDBRow className="footer pt-1">
           <MDBCol size="sm" className="text-center">
-            <h2 className="title pb-5">Siga-nos nas Redes Sociais </h2>
+            <h2 className="title pb-1">Siga nossas redes sociais </h2>
             <a href="http://www.instagram.com/fazendacoletiva">
               <img className="insta " src={insta} alt="instagram" />
             </a>
@@ -23,11 +23,7 @@ const Footer = () => {
               <img className="face" src={face} alt="facebook" />
             </a>
           </MDBCol>
-          <MDBCol size="sm" className="text-right">
-            <a href="http://www.fazendacoletiva.com">
-              <img src={logo} alt="logo" className="img-fluid logo" />
-            </a>
-          </MDBCol>
+          
         </MDBRow>
       </MDBContainer>
       <div className="footer-copyright text-center py-3">
